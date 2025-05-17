5204fe7e5ce827a35ae39fbf8656a878a6567046
riya gajbhiye <riyamp2004@gmail.com>
2025-05-18 00:55:43 +0530

    Add PDF files and update index.html

--- app.js
+++ app.js
@@ -1,50 +1,20 @@
 const express = require ("express");
 const path = require('path');
-// const { Configuration, OpenAIApi } = require('openai');
-// require('dotenv').config();
-// const fetch = require('node-fetch');
 
 
 
 const app = express();
 const port = 8080;
 
-// app.use(express.static('public'));
-// app.use(express.json());
-
-// app.post('/chat', async (req, res) => {
-//   const userMessage = req.body.message;
-
-//   const response = await fetch('http://localhost:8080/v1/chat/completions', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({
-//       model: 'mistral',
-//       messages: [{ role: 'user', content: userMessage }],
-//     }),
-//   });
-
-//   const data = await response.json();
-//   const botMessage = data.choices[0].message.content;
-//   res.json({ reply: botMessage });
-// });
-
-
-// // Middleware
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.json());
 
 
 app.set("view engine","ejs");
-// app.set("views",Path.join(__dirname,"views"));
-// app.use(express.dynamic(path.join(__dirname,"public")))
 
 app.use (express.urlencoded({extended : true}));
 app.use (express.static('public'));
 
 
 app.get("/",(req,res)=>{
-  //  res.sendFile(path.join(__dirname, 'public', 'index.html'));
    res.render("index.ejs");
 });
 
